8e57fa2970bab4d17230825b339d8c75ce7537da
hydewww <hydewwwww@163.com>
2018-05-21 23:32:58 +0800

    update

--- sql/basic_sql/data.sql
+++ sql/basic_sql/data.sql
@@ -5,7 +5,7 @@ SET FOREIGN_KEY_CHECKS=0;
 CREATE TABLE IF NOT EXISTS `a` (
   `id` varchar(4) NOT NULL,
   `flag` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `a` (`id`, `flag`) VALUES ('1', 'flag{ba5ic_iNjec7ion}');
 
